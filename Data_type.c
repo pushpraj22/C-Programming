215d0baebf02715b5639354d4332d8ddec47a5a5
Pushp Raj <pushpraj847102@gmail.com>
2026-06-09 15:58:08 +0530

    Create Data_type.c

---
+++ Data_type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    char name[10]="Pushp";
+    int age=20;
+    float height=5.6;
+    int weight=49;
+    char grade='A';
+    printf("Detail of a student\n");
+    printf("Name: %s\n",name);
+    printf("Age: %d\n",age);
+    printf("Height: %.1f\n",height);
+    printf("Weight: %d\n",weight);
+    printf("Grade: %c",grade);
+}

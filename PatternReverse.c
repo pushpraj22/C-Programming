7ebeb77a51b9287db4000e8fd420aa30e032c6d6
Pushp Raj <pushpraj847102@gmail.com>
2026-06-14 21:21:15 +0530

    Create PatternReverse.c

---
+++ PatternReverse.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int rows=5;
+    for(int i=5; i>=1; i--){
+        for(int s=0; s<rows-i; s++){
+            printf("  ");
+        }
+        for(int j=1; j<=i; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+     for(int i=2; i<=rows; i++){
+        for(int s=0; s<rows-i; s++){
+            printf("  ");
+        }
+        for(int j=1; j<=i; j++){
+            printf("* ");
+        }
+        printf ("\n");
+    }
+return 0;
+}
